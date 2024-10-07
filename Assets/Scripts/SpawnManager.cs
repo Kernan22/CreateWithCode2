e0ea4133774f1fbd3a5e198832d5d7d762a047c2
Kernan22 <pjkernan22@gmail.com>
2024-10-07 15:49:45 +0100

    Adding projectile collision, removal of animals upon collision

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,23 +9,27 @@ public class SpawnManager : MonoBehaviour
     //Set up random animal spawn
     private float spawnRangeX = 20.0f;
     private float spawnPosZ = 20.0f;
-
+    // Spawn at timed intervals
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval); 
     }
 
     // Update is called once per frame
     void Update()
     { 
-        // Animal spawn on key press S
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            // Random animal spawn
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+    
+        
+    }
+    void SpawnRandomAnimal()
+    {
+        // Random animal spawn
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
